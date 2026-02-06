34f089a1fb60cf3152a6195254cab19965309e05
Rendy247 <rasulgatin08@gmail.com>
2026-02-06 19:18:48 +0300

    Add files via upload

--- JavaScript/data.js
+++ JavaScript/data.js
@@ -4,7 +4,7 @@ const moviesDatabase = [
       title: "Звездные войны. Эпизод 1",
       type: "фильм",
       genre: "фантастика",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrL6nEowCWBlq9DWaqVMhBMV4zrQXpwngd1Q&s",
+      image: "https://images.theposterdb.com/prod/public/images/posters/optimized/movies/167/vk2WkfCyMxrcSymgMheqcyvuRDueHUgU6mYO55bW.webp",
       link: "filmlist.html?id=1",
       description: "Фильм начинается с дипломатической миссии, в которой рыцари-джедаи Квай-Гон Джинн и его ученик Оби-Ван Кеноби отправляются на планету, чтобы урегулировать торговый спор. Однако их переговоры с Торговой Федерацией терпят неудачу, и герои вынуждены спасаться бегством. Они обнаруживают, что планета подверглась блокаде и вторжению, и решают помочь местному правителю, королеве Падме Амидале, избежать захвата. Вместе они отправляются на столицу Галактической Республики, чтобы попросить помощи.",
       videoPlayers: {
@@ -20,7 +20,7 @@ const moviesDatabase = [
       title: "Звездные войны. Эпизод 2",
       type: "фильм",
       genre: "фантастика",
-      image: "https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/1216c8d2-9c47-49e9-b5c2-e8d5f01ca0ac/220x330",
+      image: "https://posters.movieposterdb.com/07_11/2002/121765/l_121765_73bcd067.jpg",
       link: "filmlist.html?id=2",
       description: "Действие разворачивается спустя десять лет после событий предыдущего фильма. Республика погружается в хаос, и тысячи планет грозят выйти из её состава. Рыцарь-джедай Оби-Ван Кеноби и его ученик Энакин Скайуокер получают задание защитить сенатора Падме Амидала от покушения. Энакин, теперь взрослый падаван, испытывает противоречивые чувства, включая любовь к Падме, что запрещено кодексом джедаев, усложняя его внутреннюю борьбу и отношения с учителем.",
       videoPlayers: {
@@ -36,7 +36,7 @@ const moviesDatabase = [
     title: "Звездные войны. Эпизод 3",
     type: "фильм",
     genre: "фантастика",
-    image: "https://avatars.mds.yandex.net/get-kinopoisk-image/4486454/2720c811-4065-43cf-9fc5-bd18ee3dbbf0/220x330",
+    image: "https://images.theposterdb.com/prod/public/images/posters/optimized/movies/169/0Io0heWrC5MZrkQMH3q8K0gEUTeLiL7DiLDuCzUJ.webp",
     link: "filmlist.html?id=3",
     description: "Фильм начинается с дипломатической миссии, в которой рыцари-джедаи Квай-Гон Джинн и его ученик Оби-Ван Кеноби отправляются на планету, чтобы урегулировать торговый спор. Однако их переговоры с Торговой Федерацией терпят неудачу, и герои вынуждены спасаться бегством. Они обнаруживают, что планета подверглась блокаде и вторжению, и решают помочь местному правителю, королеве Падме Амидале, избежать захвата. Вместе они отправляются на столицу Галактической Республики, чтобы попросить помощи.",
     videoPlayers: {
@@ -52,7 +52,7 @@ const moviesDatabase = [
        title: "Звездные войны. Эпизод 4",
        type: "фильм",
        genre: "фантастика",
-       image: "https://avatars.mds.yandex.net/get-kinopoisk-image/1600647/9bdc6690-de82-4a8c-a114-aa3a353bc1da/220x330",
+       image: "https://images.theposterdb.com/prod/public/images/posters/optimized/movies/159/M2q537D0Tnzf1xWIleSuIB3Ovmj9e5y3Dzvp6E9C.webp",
        link: "filmlist.html?id=4",
        description: "В далекой-далекой галактике, принцесса Лея, лидер повстанцев, захватывает чертежи космической станции, чтобы спасти галактику от зловещей Империи. Её корабль подвергается нападению, и она успевает спрятать данные в дроида R2-D2 вместе с просьбой о помощи. Дроид, вместе с другим дроидом - C-3PO, попадает на пустынную планету, где их находит юный фермер Люк Скайуокер. Люк вскоре узнает о своём предназначении и решает помочь в борьбе с Империей.",
        videoPlayers: {
@@ -68,7 +68,7 @@ const moviesDatabase = [
         title: "Звездные войны. Эпизод 5",
         type: "фильм",
         genre: "фантастика",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzY_6PNT0-IGeZAmj6t8Ppxl-XPjVUPhAO9g&s",
+        image: "https://xl.movieposterdb.com/05_06/1980/0080684/xl_22470_0080684_b87199b8.jpg",
         link: "filmlist.html?id=5",
         description: "Действие фильма разворачивается во время Галактической гражданской войны. Повстанцы, скрывающиеся на ледяной планете, подвергаются атаке со стороны своих врагов. Герои вынуждены спасаться бегством. Один из них отправляется на поиски великого учителя, чтобы обучиться искусству, которое поможет ему в борьбе. В это время другой герой оказывается в плену у антагониста, который преследует свои зловещие цели.",
         videoPlayers: {
@@ -84,7 +84,7 @@ const moviesDatabase = [
         title: "Звездные войны. Эпизод 6",
         type: "фильм",
         genre: "фантастика",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwMcTiuDR8NxoZp762rZiDiqRLKaOuy4ZxEA&s",
+        image: "https://posters.movieposterdb.com/24_05/1983/86190/l_star-wars-episode-vi-return-of-the-jedi-movie-poster_3782f5b6.jpg",
         link: "filmlist.html?id=6",
         description: "Фильм начинается с того, что Люк Скайуокер и его друзья предпринимают смелую операцию по спасению Хана Соло из лап преступного лорда Джаббы Хатта. Они проникают в его дворец на Татуине, где их ждет множество опасностей и хитрых ловушек. В итоге, благодаря быстроте ума и боевым навыкам, герои успешно освобождают Хана и уничтожают Джаббу, после чего отправляются на встречу с Альянсом повстанцев, готовящимся к решающей битве.",
         videoPlayers: {
@@ -100,7 +100,7 @@ const moviesDatabase = [
         title: "Звездные войны. Эпизод 7",
         type: "фильм",
         genre: "фантастика",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7LFJcqvaiQXJ1qlRRjP0N1TOc4nbRKsRD8w&s",
+        image: "https://posters.movieposterdb.com/15_11/2015/2488496/l_2488496_0dedc25d.jpg",
         link: "filmlist.html?id=7",
         description: "Действие происходит спустя тридцать лет после падения Империи. Новая угроза в виде Первого Ордена, возглавляемого таинственным лидером, угрожает галактике. Поиски последнего джедая заставляют Лея Органу отправить лучшего пилота По Дэмерона на миссию. По находит важную информацию, но попадает в плен к Кайло Рену. Рен — мощный темный воин, стремящийся уничтожить Сопротивление.",
         videoPlayers: {
@@ -116,7 +116,7 @@ const moviesDatabase = [
         title: "Звездные войны. Эпизод 8",
         type: "фильм",
         genre: "фантастика",
-        image: "https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/e9bc098b-43fd-446a-a3dd-9b37b8e2a8ec/220x330",
+        image: "https://posters.movieposterdb.com/20_10/2017/2527336/l_2527336_0a953b05.jpg",
         link: "filmlist.html?id=8",
         description: "События новой главы саги разворачиваются после гибели Хана Соло. В Галактике продолжается ожесточённая война: Первый Орден давит всё сильнее, а Сопротивление пытается удержать надежду на победу. Рей открыла в себе Силу и стремится понять своё предназначение, но ответы могут оказаться опаснее, чем она ожидает. Её путь приводит к Люку Скайуокеру — последнему джедаю, который скрывался от мира и теперь должен решить, готов ли снова вмешаться в ход истории. Тем временем Кайло Рен, окончательно выбравший Тёмную сторону и занявший высокое положение в Ордене, всё глубже погружается в свои амбиции. Лея вместе с По, Финном и BB-8 отправляется на рискованную миссию, способную изменить исход войны.",
         videoPlayers: {
@@ -132,7 +132,7 @@ const moviesDatabase = [
         title: "Звездные войны. Эпизод 9",
         type: "фильм",
         genre: "фантастика",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLqWHKUcQvgIBXaAbeIjIv1NP8P8_3cNi5uA&s",
+        image: "https://posters.movieposterdb.com/22_06/2019/2527338/l_2527338_cb2a959b.jpg",
         link: "filmlist.html?id=9",
         description: "Фильм завершает невероятную историю семьи Скайуокеров, длящуюся уже более сорока лет, и обещает дать ответы на все загадки из предыдущих серий. Зрителя ожидают старые и новые герои, уникальные миры, увлекательные путешествия на край Галактики и грандиозный финал фантастической саги.",
         videoPlayers: {
@@ -148,7 +148,7 @@ const moviesDatabase = [
         title: "Мондалорец",
         type: "сериал",
         genre: "фантастика",
-        image: "https://www.film.ru/sites/default/files/styles/thumb_260x400/public/movies/posters/40966273-2423940.jpg",
+        image: "https://posters.movieposterdb.com/23_01/2019/8111088/l_the-mandalorian-movie-poster_7f3ed940.jpg",
         link: "filmlist.html?id=10",
         description: "После падения Империи одинокий мандалорец-наёмник Дин Джарин бороздит дальние рубежи Галактики. Взявшись за опасный заказ, он встречает загадочного малыша Грогу и вместо убийства решает защитить его от остатков имперских сил, становясь беглецом и отцом для существа, связанного с Силой. Это история о чести, долге и искуплении.",
         videoPlayers: {
@@ -162,9 +162,9 @@ const moviesDatabase = [
       {
         id: 11,
         title: "Звездные войны: Войны клонов",
-        type: "сериал",
+        type: "мультфильм",
         genre: "фантастика",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQY3GyijKtTMobsvDWciWdL2wqSX_Kf5BgKAg&s",
+        image: "https://posters.movieposterdb.com/08_07/2008/1185834/l_1185834_d7fa60b1.jpg",
         link: "filmlist.html?id=11",
         description: "На передовых позициях межгалактической войны Добра и Зла - снова любимые герои: Анакин Скайуокер, Оби-Ван Кеноби и Падме Амидала, а также присоединившийся к ним падаван Анакина, Асока.Противостоящие им злодеи - Палпатин, граф Дуку и генерал Гривус - вынашивают план захвата Галактики. Начинается последняя схватка, и судьбы Вселенной вверены рыцарям-джедаям.",
         videoPlayers: {
@@ -174,6 +174,70 @@ const moviesDatabase = [
           trailer: ""
         },
         backgroundImage: "https://static.okko.tv/images/v4/53f472dc-dadc-4e84-9f8c-3d52f07e641b?scale=1&quality=80"
+      },
+      {
+        id: 12,
+        title: "Звездные войны: Повстанцы",
+        type: "мультфильм",
+        genre: "фантастика",
+        image: "https://posters.movieposterdb.com/14_07/2014/2930604/l_2930604_c1b1e85a.jpg",
+        link: "filmlist.html?id=12",
+        description: "Для Галактики настали тёмные времена. Поглощая один мир за другим, Империя продолжает наращивать свою мощь. Имперские войска, оккупировавшие отдаленную планету, устанавливают на ней деспотический режим. Но находятся смельчаки, готовые противостоять силам Империи. Это экипаж космического корабля «Призрак»! Команде повстанцев предстоит столкнуться лицом к лицу с новыми грозными противниками и стать героями, способными поднять восстание.",
+        videoPlayers: {
+          1: "https://api.variyt.ws/embed/movie/9957",
+          2: "https://gencit.info/low/803148?season=1&episode=1",
+          3: "",
+          trailer: ""
+        },
+        backgroundImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6swyKro9qE6ETHbRumRFoD6grGbqNsNjACg&s"
+      },
+      {
+        id: 13,
+        title: "Звездные войны: Сказания о джедаях",
+        type: "мультфильм",
+        genre: "фантастика",
+        image: "https://avatars.mds.yandex.net/get-kinopoisk-image/1898899/46ea369a-9b43-45aa-ab00-bb5e8b4408fe/600x900",
+        link: "filmlist.html?id=13",
+        description: "Энакин Скайуокер, отец будущего знаменитого джедая Люка Скайуокера еще молод и только начинает свой путь. Он походит обучение у Оби-Вана и мастера Йоды. Но привычный мир неотвратимо меняется. Рушится Орден, набирает силы Империя, Галактическая Республика вязнет в коррупции.",
+        videoPlayers: {
+          1: "https://api.variyt.ws/embed/movie/59706",
+          2: "",
+          3: "",
+          trailer: ""
+        },
+        backgroundImage: "https://www.film.ru/sites/default/files/styles/thumb_1024x450/public/trailers_frame/tales-of-the-jedi-season-1-trailer-1.jpg"
+      },
+      {
+        id: 14,
+        title: "Звездные войны: Бракованная партия",
+        type: "мультфильм",
+        genre: "фантастика",
+        image: "https://posters.movieposterdb.com/24_02/2021/12708542/l_star-wars-the-bad-batch-movie-poster_161160c8.jpg",
+        link: "filmlist.html?id=14",
+        description: "«Плохая партия» элитных и экспериментальных клонов пробивается через постоянно меняющуюся галактику сразу после Войн клонов. Эти герои впервые будут показаны во вселенной Звёздных войн.",
+        videoPlayers: {
+          1: "https://api.variyt.ws/embed/movie/46140",
+          2: "https://gencit.info/low/1392645?season=1&episode=1",
+          3: "",
+          trailer: ""
+        },
+        backgroundImage: "https://static.okko.tv/images/v4/decaa622-ac39-48d9-bd65-2ecaf21b5bdc?scale=1&quality=80"
+      },
+      {
+        id: 15,
+        title: "Изгой-один: Звёздные Войны",
+        type: "фильм",
+        genre: "фантастика",
+        image: "https://posters.movieposterdb.com/20_02/2016/3748528/l_3748528_a7e1b48f.jpg",
+        link: "filmlist.html?id=15",
+        description: "Сопротивление собирает отряд для выполнения особой миссии - надо выкрасть чертежи самого совершенного и смертоносного оружия Империи. Возглавляет бойцов неуправляемая и бесстрашная Джин Эрсо, у которой в этом самоубийственном задании есть и личные мотивы. Не всем суждено вернуться домой, но герои готовы к этому, ведь на кону судьба Галактики.",
+        videoPlayers: {
+          1: "https://api.variyt.ws/embed/movie/88",
+          2: "https://vkvideo.ru/video_ext.php?oid=-34229261&id=456250613&hash=baec40211726aa7f&hd=3",
+          3: "",
+          trailer: ""
+        },
+        backgroundImage: "https://static.okko.tv/images/v4/11060d47-b059-488f-ad21-e811b045cea2?scale=1&quality=80"
       },
 ];
 window.moviesDatabase = moviesDatabase;
